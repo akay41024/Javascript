32fb34a330ec708eb4ab161596e493b2d073e9f1
Aasif Khan <83283933+akay41024@users.noreply.github.com>
2023-09-15 16:13:06 +0000

    Array complete

--- Basics_02/array_02.js
+++ Basics_02/array_02.js
@@ -11,3 +11,25 @@ console.log(all_heros);
 const all_new_heros = [...marvel_heros, ...dc_heros]
 console.log(all_new_heros);
 
+
+const another_array = [1,2,3,[4,5,6,7],[8,9]]
+
+console.log(another_array.flat(Infinity))
+
+
+console.log(Array.isArray("Aasif"))
+
+console.log(Array.from("Aasif"))
+
+console.log(Array.from({
+    name: "Aasif"
+})) //we need to give the tell the javascript which will be converted key or values that's why the outpu is empty array
+
+let arr1=100
+let arr2=200
+let arr3=300
+
+console.log(Array.of(arr1,arr2,arr3))
+
+
+
