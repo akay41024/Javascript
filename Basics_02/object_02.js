6cb8a433c9f89696df7645458d06a4d5b0f71578
= <=>
2023-09-16 15:44:31 +0530

    JSON understanding

--- Basics_02/object_02.js
+++ Basics_02/object_02.js
@@ -20,7 +20,7 @@ const  regularUser = {
 }
 
 // console.log(regularUser.fullname?.userfullname.firstname); // this is for extra protection if fullname is not exist
-console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname.userfullname);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
@@ -48,11 +48,47 @@ const users = [
     },
 ]
 
-console.log(users[1].email);
-console.log(instaUser);
+// console.log(users[1].email);
+// console.log(instaUser);
 
-console.log(Object.keys(instaUser)); //In this we assign the object in array so that we modify easily
-console.log(Object.values(instaUser));
-console.log(Object.entries(instaUser)); //In this we assign every key in array by doing this entries keyword
+// console.log(Object.keys(instaUser)); //In this we assign the object in array so that we modify easily
+// console.log(Object.values(instaUser));
+// console.log(Object.entries(instaUser)); //In this we assign every key in array by doing this entries keyword
 
-console.log(instaUser.hasOwnProperty("isLoggedIn")); //that's give us true or false if the proprty exist give us true otherwise false
+// console.log(instaUser.hasOwnProperty("isLoggedIn")); //that's give us true or false if the proprty exist give us true otherwise false
+// de structure in objects
+
+
+const course = {
+    coursename: "Full stack",
+    price: "999",
+    courseInstructure: "Aasif khan"
+}
+
+// const {courseInstructure,coursename,price} = course
+
+// console.log(courseInstructure);
+
+const {courseInstructure: instructor} = course // In this we change the courseInstructure name to instructor
+console.log(instructor);
+
+
+
+// JSON formate example
+// {
+//     "name": "Aasif",
+//     "coursename": "JS",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
+
+
+
+
