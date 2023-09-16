a5635d43efa066408e1a720f469d1337c6482eb8
= <=>
2023-09-16 15:21:03 +0530

    objects 02 file adding some main points

--- Basics_02/object_02.js
+++ Basics_02/object_02.js
@@ -7,4 +7,52 @@ instaUser.email = "a@gmail.com",
 instaUser.name = "Aasif",
 instaUser.isLoggedIn = false
 
-console.log(instaUser)
+// console.log(instaUser)
+
+const  regularUser = {
+    email: "abc@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: "Aasif",
+            lastname: "Khan"
+        }
+    }
+}
+
+// console.log(regularUser.fullname?.userfullname.firstname); // this is for extra protection if fullname is not exist
+console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj4 = {5: "e", 6: "f"}
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4)  //this is first way to merge to or more objects 
+// console.log(obj3);
+
+const obj3 = {...obj1,...obj2,...obj4}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "hd@gmail.com",
+    },
+    {
+        id: 1,
+        email: "hd@gmail.com",
+    },
+    {
+        id: 1,
+        email: "hd@gmail.com",
+    },
+]
+
+console.log(users[1].email);
+console.log(instaUser);
+
+console.log(Object.keys(instaUser)); //In this we assign the object in array so that we modify easily
+console.log(Object.values(instaUser));
+console.log(Object.entries(instaUser)); //In this we assign every key in array by doing this entries keyword
+
+console.log(instaUser.hasOwnProperty("isLoggedIn")); //that's give us true or false if the proprty exist give us true otherwise false
