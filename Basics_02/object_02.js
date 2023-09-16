920585aae7be8522833ea30d45befa0e65921f03
Aasif Khan <83283933+akay41024@users.noreply.github.com>
2023-09-16 09:15:29 +0000

    object 2 file

---
+++ Basics_02/object_02.js
@@ -0,0 +1,10 @@
+// const instaUser = new Object() //single ton object 
+
+const instaUser = {}
+
+instaUser.id = 1,
+instaUser.email = "a@gmail.com",
+instaUser.name = "Aasif",
+instaUser.isLoggedIn = false
+
+console.log(instaUser)
