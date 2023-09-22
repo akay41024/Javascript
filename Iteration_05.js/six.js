1a6294987b07b0b4828863eb55f127876e631376
= <=>
2023-09-22 18:52:36 +0530

    complete six file content

--- Iteration_05.js/six.js
+++ Iteration_05.js/six.js
@@ -43,13 +43,19 @@ const books = [
   ];
 
 
-  let userBooks = books.filter((bk) =>{
-    return bk.genre === "History"
-  })
+  // let userBooks = books.filter((bk) =>{
+  //   return bk.genre === "History"
+  // }) 
+
+  let userBooks = books.filter((bk) =>bk.genre === "History")
 
 //   console.log(userBooks);
 
   userBooks = books.filter((bk) =>{
-    return bk.publish >= 2000
+    return bk.publish >= 1995 && bk.genre === "History"
   })
+  // userBooks = books.filter((bk) =>{
+  //   return bk.publish >= 2000
+  // })
   console.log(userBooks);
+
